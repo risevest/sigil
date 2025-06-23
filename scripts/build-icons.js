0cf74ffe1f4aeb607c3de820ab79d01b1c86e0bd
Tola Abiodun <adetolaabiodun@outlook.com>
2025-06-23 19:37:37 +0100

    feat: adds lucide icon for HISA system (#19)
    * adds lucide icon for HISA system
    
    * Update scripts/build-icons.js
    
    Co-authored-by: BalogunofAfrica <45393944+BalogunofAfrica@users.noreply.github.com>
    
    * Update scripts/build-icons.js
    
    Co-authored-by: BalogunofAfrica <45393944+BalogunofAfrica@users.noreply.github.com>
    
    * regenerates icon pack
    
    ---------
    
    Co-authored-by: BalogunofAfrica <45393944+BalogunofAfrica@users.noreply.github.com>

--- scripts/build-icons.js
+++ scripts/build-icons.js
@@ -48,6 +48,18 @@ const assetsFiles = [
     index: path.join(rootDir, "mgc.ts"),
     out: ["icon-pack", "mgc"],
   },
+  {
+    attr: (name) => `/**
+         * Lucide Icon: ${toTitleCase(name)}
+         * @see {@link https://lucide.dev/icons/${name} Lucide Icon Docs}
+         */`,
+    dir: path.join(rootDir, "icons", "lucide"),
+    id: "lucide",
+    prefix: "Lc",
+    index: path.join(rootDir, "lucide.ts"),
+    isStrokeIcon: true,
+    out: ["icon-pack", "lucide"],
+  },
 ];
 
 function isNumber(str) {
