f6557c799d91bf7d978de7b68ae898da4c5d4524
Allison Kosy <Allisonkosy@gmail.com>
2026-06-29 14:55:04 +0100

    feat: add Exchange Naira and Target icons to remix (#29)

---
+++ packages/sicily/icon-pack/remix/exchange-naira-line.tsx
@@ -0,0 +1,29 @@
+import { memo } from 'react'
+import type { IconProps } from '../../types'
+const ExchangeNairaLine = /* @__PURE__ */ memo(function ExchangeNairaLine(
+  _props: IconProps
+) {
+  const props = _props
+  return (
+    <svg fill="currentColor" viewBox="0 0 24 24" width="1em" height="1em" {...props}>
+      <g clipPath="url(#a)">
+        <path d="M16.146 12.584A6.667 6.667 0 0 0 6.692 4.21l-.827-1.447a8.33 8.33 0 0 1 8.301.02 8.334 8.334 0 0 1 3.431 10.642l1.119.645-3.471 1.845-.138-3.929zM3.854 7.413a6.668 6.668 0 0 0 9.454 8.375l.827 1.447a8.33 8.33 0 0 1-8.302-.02 8.334 8.334 0 0 1-3.43-10.641l-1.12-.645 3.471-1.845.138 3.929z" />
+        <path
+          fillRule="evenodd"
+          d="m13.057 9.052-.008-2.534h-1.666v3.48l.02 1.066h-.01l-.485-1.055-1.772-3.491H6.953l.008 2.534H5.79l-.01 1.61h1.171v2.817H8.62v-3.301l-.031-1.224h.02l.56 1.234 1.687 3.291h2.194v-2.817h1.16l.008-1.61z"
+          clipRule="evenodd"
+        />
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path fill="none" d="M0 0h24v24H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+})
+/**
+ * Remix Icon: Exchange Naira Line
+ * @see {@link https://remixicon.com/icon/exchange-naira-line Remix Icon Docs}
+ */
+export { ExchangeNairaLine }
---
+++ packages/sicily/icon-pack/remix/target-line.tsx
@@ -0,0 +1,15 @@
+import { memo } from 'react'
+import type { IconProps } from '../../types'
+const TargetLine = /* @__PURE__ */ memo(function TargetLine(_props: IconProps) {
+  const props = _props
+  return (
+    <svg fill="currentColor" viewBox="0 0 24 24" width="1em" height="1em" {...props}>
+      <path d="M10 1.668a.833.833 0 1 1 0 1.667A6.667 6.667 0 1 0 16.667 10a.833.833 0 1 1 1.667 0A8.333 8.333 0 1 1 10 1.668m0 3.333a.833.833 0 1 1 0 1.667 3.333 3.333 0 1 0 3.334 3.333.833.833 0 1 1 1.666 0 5 5 0 1 1-5-5m5.476-3.25c.23 0 .417.187.417.417v1.525c0 .23.187.416.417.416h1.523c.23 0 .417.186.417.416v.763l-1.574 1.574a1.67 1.67 0 0 1-1.178.488H13.83l-3.242 3.24a.833.833 0 0 1-1.178-1.178l3.242-3.24V4.505c0-.442.175-.866.487-1.179l1.575-1.574z" />
+    </svg>
+  )
+})
+/**
+ * Remix Icon: Target Line
+ * @see {@link https://remixicon.com/icon/target-line Remix Icon Docs}
+ */
+export { TargetLine }
